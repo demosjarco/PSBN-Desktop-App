6d783c80b020dd1194db3e0a84e430cf42cb7c41
demosjarco <demosjarco@users.noreply.github.com>
2019-06-23 11:24:24 -0700

    Added account id for the current schools

--- livestreamApi.js
+++ livestreamApi.js
@@ -1,3 +1,14 @@
 'use strict';
 
+// owner_account_id
+const accountIds = {
+	chs: 21054388,
+	cehs: 14884070,
+	ihs: 27655587,
+	lhs: 9697821,
+	phs: 25780015,
+	rsk: 5145446,
+	smhs: 27655594
+}
+
 const { ipcMain } = require('electron');
